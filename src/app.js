1ff7d1e3630c47e5f2988f63e71fb4cda2726eb5
palat2000 <palat.tungsantitham@gmail.com>
2023-09-28 11:59:56 +0700

    init config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const cors = require("cors");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+const rateLimitMiddleware = require("./middlewares/rate-limit");
 
 app.use(cors());
-
+app.use(morgan("dev"));
+app.use(rateLimitMiddleware);
 app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
